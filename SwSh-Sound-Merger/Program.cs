2a94b395953b189cb4174bb4c73f91152438e99f
Sun <gabehttps@gmail.com>
2019-11-18 21:34:17 -0800

    Improve and fix lots of bugs

--- SwSh-Sound-Merger/Program.cs
+++ SwSh-Sound-Merger/Program.cs
@@ -6,7 +6,7 @@ using System.IO;
 using VGAudio.Containers.NintendoWare;
 using VGAudio.Containers.Wave;
 using VGAudio.Formats;
-using VGAudio.Utilities;
+using static SwSh_Sound_Merger.Utils;
 
 namespace SwSh_Sound_Merger
 {
@@ -18,14 +18,46 @@ namespace SwSh_Sound_Merger
         {
             if (args.Length < 1 && args.Length > 3)
             {
-                Console.WriteLine("Arguments: <Path to Sounds>, <Overwrite=false>, <Dictionary=CurrentDirectory/Dictonary.txt>");
+                Console.WriteLine("Arguments: <Path to Sounds>, <Format=bfstm:16:le:ca>, <Overwrite=false>, <Dictionary=CurrentDirectory/Dictonary.txt>");
+                Console.WriteLine("Format Options: bfstm, bcstm : ima, 4, 8, 16:le (switch), be (wii u) : ca (wii u/switch), ct (3ds), re");
                 Console.Write("Press any key to continue!");
                 Console.ReadKey(true);
                 return;
             }
+            AudioSpecs specs;
+            if (args.Length >= 2)
+            {
+                specs = new AudioSpecs(args[1]);
+
+                if (specs.HasInvalidOptions)
+                {
+                    Console.WriteLine("Invalid format options!");
+                    Console.WriteLine("Format Options: bfstm, bcstm, brstm : ima, 4, 8, 16:le (switch), be (wii u) : ca (wii u/switch), ct (3ds), re");
+                    Console.Write("Press any key to continue!");
+                    Console.ReadKey(true);
+                    return;
+                }
+            }
+            else
+            {
+                specs = new AudioSpecs("bfstm:16:le:ca");
+            }
+
+            bool Overwrite = false;
+            if (args.Length >= 3)
+            {
+                if (!bool.TryParse(args[2], out Overwrite))
+                {
+                    Console.WriteLine("Overwrite argument is invalid!");
+                    Console.Write("Press any key to continue!");
+                    Console.ReadKey(true);
+                    return;
+                }
+            }
+
             FileInfo dictionary;
-            if (args.Length == 3)
-                dictionary = new FileInfo(args[2]);
+            if (args.Length >= 4)
+                dictionary = new FileInfo(args[3]);
             else
                 dictionary = CurrentDirectory.GetFile("Dictionary.txt");
 
@@ -35,18 +67,6 @@ namespace SwSh_Sound_Merger
                 Console.Write("Press any key to continue!");
                 Console.ReadKey(true);
                 return;
-            }
-
-            bool Overwrite = false;
-            if (args.Length >= 2)
-            {
-                if (!bool.TryParse(args[1], out Overwrite))
-                {
-                    Console.WriteLine("2nd argument is invalid!");
-                    Console.Write("Press any key to continue!");
-                    Console.ReadKey(true);
-                    return;
-                }
             }
 
             DirectoryInfo gameSounds = new DirectoryInfo(args[0]);
@@ -66,24 +86,28 @@ namespace SwSh_Sound_Merger
                 if (sound.StartFileIndex == -1 && sound.LoopFileIndex == -1)
                     continue;
 
-                
-                FileInfo outFile = CurrentDirectory.GetDirectory("out").GetFile($"{sound.Name}.bfstm");
+
+                FileInfo outFile = CurrentDirectory.GetDirectory("out").GetFile($"{sound.Name}.{specs.type.ToString().ToLower()}");
                 if (!Overwrite && outFile.Exists)
                     continue;
 
                 FileInfo tempWave = new FileInfo(Path.GetTempFileName());
 
                 WaveReader reader = new WaveReader();
-                BCFstmWriter writer = new BCFstmWriter(NwTarget.Cafe);
-                writer.Configuration.Endianness = Endianness.LittleEndian;
                 AudioData audio = null;
 
                 Console.WriteLine($"Processing {sound.Name}...");
                 if (sound.StartFileIndex != -1 && sound.LoopFileIndex == -1)
                 {
-                    VorbisWaveReader vorbisStart = new VorbisWaveReader(gameSounds.GetFile(sound.StartFileName).FullName);
-                    WaveFileWriter.CreateWaveFile(tempWave.FullName, vorbisStart.ToWaveProvider16());
-                    audio = reader.Read(tempWave.OpenRead());
+                    using (VorbisWaveReader vorbisStart = new VorbisWaveReader(gameSounds.GetFile(sound.StartFileName).FullName))
+                    {
+                        WaveFileWriter.CreateWaveFile(tempWave.FullName, vorbisStart.ToWaveProvider16());
+                    }
+                    using (FileStream stream = tempWave.OpenRead())
+                    {
+                        audio = reader.Read(stream);
+                    }
+
                 }
                 else if (sound.StartFileIndex != -1 && sound.LoopFileIndex != -1)
                 {
@@ -95,11 +119,14 @@ namespace SwSh_Sound_Merger
                     VorbisReader dataLoop = new VorbisReader(gameSounds.GetFile(sound.LoopFileName).FullName);
                     int startLoop = (int)dataStart.TotalSamples;
                     int endLoop = (int)dataStart.TotalSamples + (int)dataLoop.TotalSamples;
+                    vorbisStart.Dispose();
+                    vorbisLoop.Dispose();
                     dataStart.Dispose();
                     dataLoop.Dispose();
-
-                    WaveReader waveReader = new WaveReader();
-                    audio = waveReader.Read(tempWave.OpenRead());
+                    using (FileStream stream = tempWave.OpenRead())
+                    {
+                        audio = reader.Read(stream);
+                    }
 
                     audio.SetLoop(true, startLoop, endLoop);
                 }
@@ -109,14 +136,23 @@ namespace SwSh_Sound_Merger
                     WaveFileWriter.CreateWaveFile(tempWave.FullName, vorbisLoop.ToWaveProvider16());
                     VorbisReader dataLoop = new VorbisReader(gameSounds.GetFile(sound.LoopFileName).FullName);
                     int totalSamples = (int)dataLoop.TotalSamples;
+                    vorbisLoop.Dispose();
                     dataLoop.Dispose();
-                    WaveReader waveReader = new WaveReader();
-                    audio = waveReader.Read(tempWave.OpenRead());
+                    audio = reader.Read(tempWave.OpenRead());
 
                     audio.SetLoop(true, 0, totalSamples);
                 }
+
                 if (audio != null)
-                    writer.WriteToStream(audio, outFile.Open(FileMode.Create));
+                {
+                    BCFstmWriter writer = new BCFstmWriter(specs.target.Value);
+                    writer.Configuration.Codec = specs.encoding.Value;
+                    writer.Configuration.Endianness = specs.endianness.Value;
+                    using (FileStream stream = outFile.OpenWrite())
+                    {
+                        writer.WriteToStream(audio, stream);
+                    }
+                }
             }
             Console.WriteLine("Succesfully merged and convertered all tracks!");
             Console.Write("Press any key to continue!");
